fe1b04a48aa0de015ae39909dc75b55bfcb9e3a1
Bartek Z <bartekzarzyk@gmail.com>
2022-07-12 22:20:17 +0200

    add code day 3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,8 @@ import {useNavigate} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {
     faBed,
-    faPlane,
-    faCar,
-    faLocationDot,
-    faTaxi,
+    faPhone,
+    faMapLocationDot,
     faCalendarDays,
     faPerson
 } from '@fortawesome/free-solid-svg-icons';
@@ -59,25 +57,21 @@ const Header = ({type}) => {
         <header className='header'>
             <div className={ type ==='list' ? "headerContainer listMode" : "headerContainer"}>
                 <div className='headerList'>
-                    <div className="headerListItem active">
+                    <div className='headerListStart'>
+                        <div className="headerListItem active">
                         <FontAwesomeIcon icon={faBed}/>
-                        <span>Stays</span>
+                        <span>Noclegi</span>
+                        </div>
                     </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faPlane}/>
-                        <span>Flights</span>
-                    </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faCar}/>
-                        <span>Car rentals</span>
-                    </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faLocationDot}/>
-                        <span>Attractions</span>
-                    </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faTaxi}/>
-                        <span>Airport taxis</span>
+                    <div className='headerListEnd' >
+                        <div className="headerListItem active">
+                            <FontAwesomeIcon icon={faPhone}/>
+                            <span>Kontakt: <a href="tel:+123 456 789">+48 123 456 789</a></span>
+                        </div>
+                        <div className="headerListItem active">
+                            <FontAwesomeIcon icon={faMapLocationDot}/>
+                            <span>Zobacz na mapie</span>
+                        </div>  
                     </div>
                 </div>
                 {type !== 'list' && 
--- src/pages/SingleHotelPage/Hotel.jsx
+++ src/pages/SingleHotelPage/Hotel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import Header from '../../components/header/Header.jsx';
 import Form from '../../components/form/Form.jsx';
@@ -10,34 +10,62 @@ import {faLocationDot, faBus, faCircleArrowLeft, faCircleArrowRight, faCircleXma
 import './css/Hotel.css'
 
 const Hotel = () => {
-    const[slideNumber, setSlideNumber] = useState(0);
-    const[openSlider, setOpenSlider] = useState(false);
 
-    const handleChangeImage = (index) => {
+    const [slideNumber,
+        setSlideNumber] = useState(0);
+    const [openSlider,
+        setOpenSlider] = useState(false);
+
+    const handleOpenImage = (index) => {
         setSlideNumber(index);
         setOpenSlider(!openSlider);
     }
 
+    const hnadleMoveImage = (direction) => {
+        let newSlideNumber;
+
+        if (direction === 'left') {
+            newSlideNumber = slideNumber === 0
+                ? photos.length - 1
+                : slideNumber - 1;
+        } else {
+            newSlideNumber = slideNumber === photos.length - 1
+                ? 0
+                : slideNumber + 1;
+        }
+        setSlideNumber(newSlideNumber);
+
+    }
+
     const photos = [
         {
-            src: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1502672023488-70e25813eb80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1622866306950-81d17097d458?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1697&q=80' }, 
-        {
-            src: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80' }, 
-      
+            src: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?ixlib=rb-1.2.1&ixid=Mn' +
+                    'wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1502672023488-70e25813eb80?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1622866306950-81d17097d458?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1697&q=80'
+        }, {
+            src: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid' +
+                    '=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80'
+        }
     ];
 
     return (
@@ -46,13 +74,25 @@ const Hotel = () => {
             <Header type='list'/>
             <div className='hotelContainer'>
                 {openSlider && <div className="slider">
-                    <FontAwesomeIcon icon={faCircleXmark}/>
-                    <FontAwesomeIcon icon={faCircleArrowLeft}/>
-                    <FontAwesomeIcon icon={faCircleArrowRight}/>
+                    <FontAwesomeIcon
+                        icon={faCircleXmark}
+                        className='sliderClose'
+                        onClick={() => setOpenSlider(!openSlider)}/>
+                    <FontAwesomeIcon
+                        icon={faCircleArrowLeft}
+                        className='sliderArrow arrowLeft'
+                        onClick={() => hnadleMoveImage('left')}/>
+                    <FontAwesomeIcon
+                        icon={faCircleArrowRight}
+                        className='sliderArrow arrowRight'
+                        onClick={() => hnadleMoveImage('right')}/>
                     <div className="sliderWrapper">
-                        <img src={photos[slideNumber].src} alt="" />
+                        <img
+                            src={photos[slideNumber].src}
+                            alt="apartament-img"
+                            className='sliderImage'/>
                     </div>
-                    </div>}
+                </div>}
                 <div className='hotelWrapper'>
                     <button className='hotelWrapperBookNow'>Zarezerwuj!</button>
                     <h2 className='hotelTitle'>Jakiś tam apartament - Tytuł</h2>
@@ -65,24 +105,37 @@ const Hotel = () => {
                     <div className='hotelImages'>
                         {photos.map((photo, index) => (
                             <div className="hotelImgWrapper" key={index}>
-                                <img 
-                                key={index}
-                                onClick={() => handleChangeImage(index)}
-                                src={photo.src} 
-                                alt='img-house' 
-                                className='hotelImage'/>
+                                <img
+                                    key={index}
+                                    onClick={() => handleOpenImage(index)}
+                                    src={photo.src}
+                                    alt='img-house'
+                                    className='hotelImage'/>
                             </div>
                         ))}
                     </div>
                     <div className="hotelDetails">
                         <div className="hotelDetailsText">
                             <h3>Opis Hotelu</h3>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus debitis harum sunt repellendus sed et non ullam ab blanditiis nisi consectetur, incidunt alias, exercitationem reiciendis id explicabo maiores ipsum illum quod? Architecto quia, unde deleniti velit itaque saepe corrupti sapiente, delectus, illo quibusdam repellat quisquam. Consectetur, dolorem eos omnis vitae, quae optio nihil officiis pariatur dolore ex consequatur deserunt at rem laudantium numquam quisquam, sed a distinctio temporibus ipsum molestias. Numquam eius molestias sunt maiores explicabo amet vitae nulla velit sit doloremque quisquam, aut atque hic architecto, odio vel obcaecati nostrum laboriosam? Obcaecati, mollitia cum nisi deserunt necessitatibus odio adipisci!</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus debitis
+                                harum sunt repellendus sed et non ullam ab blanditiis nisi consectetur, incidunt
+                                alias, exercitationem reiciendis id explicabo maiores ipsum illum quod?
+                                Architecto quia, unde deleniti velit itaque saepe corrupti sapiente, delectus,
+                                illo quibusdam repellat quisquam. Consectetur, dolorem eos omnis vitae, quae
+                                optio nihil officiis pariatur dolore ex consequatur deserunt at rem laudantium
+                                numquam quisquam, sed a distinctio temporibus ipsum molestias. Numquam eius
+                                molestias sunt maiores explicabo amet vitae nulla velit sit doloremque quisquam,
+                                aut atque hic architecto, odio vel obcaecati nostrum laboriosam? Obcaecati,
+                                mollitia cum nisi deserunt necessitatibus odio adipisci!</p>
                         </div>
                         <div className="hotelDetailsPrice">
                             <h3>Idealny na 7 dni pobytu!</h3>
-                            <span>Podsumowanie opisu - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto.</span>
-                            <h4><b>900zł</b> <small>(7 dni)</small> </h4>
+                            <span>Podsumowanie opisu - Lorem, ipsum dolor sit amet consectetur adipisicing
+                                elit. Architecto.</span>
+                            <h4>
+                                <b>900zł</b>
+                                <small> (7 dni)</small>
+                            </h4>
                             <button>Zarezerwuj!</button>
                         </div>
                     </div>
